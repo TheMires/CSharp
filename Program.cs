60dbda9eea39847c4b93f493f1ea457201e3e118
TheMires <thamiresfreitas051@gmail.com>
2022-04-08 22:10:05 -0300

    @TheMires ClassesExercicio

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using auladeclasses.src;
+using System;
+
+namespace auladeclasses
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Os Simpsons");
+            Console.WriteLine();
+            PersonagensSimpsons Homer = new PersonagensSimpsons("Homer", 39, "preto, quase careca");
+            PersonagensSimpsons Marge = new PersonagensSimpsons("Marge", 38, "azul");
+            PersonagensSimpsons Lisa = new PersonagensSimpsons("Lisa", 8, "amarelo");
+            PersonagensSimpsons Bart = new PersonagensSimpsons("Bart", 10, "amarelo");
+            PersonagensSimpsons Maggie = new PersonagensSimpsons("Maggie", 1, "amarelo");
+
+            Console.WriteLine($"O pai é o {Homer.Nome}, com {Homer.Idade} anos de idade e cabelo {Homer.Cabelo}");
+            Console.WriteLine($"A mãe é a {Marge.Nome}, com {Marge.Idade} anos de idade e cabelo {Marge.Cabelo}");
+            Console.WriteLine($"A filha é a {Lisa.Nome}, com {Lisa.Idade} anos de idade e cabelo {Lisa.Cabelo}");
+            Console.WriteLine($"O filho é o {Bart.Nome}, com {Bart.Idade} anos de idade e cabelo {Bart.Cabelo}");
+            Console.WriteLine($"A filha é a {Maggie.Nome}, com {Maggie.Idade} ano de idade e cabelo {Maggie.Cabelo}");
+
+            Console.WriteLine();
+
+            Homer.beber (" cerveja Duff");
+            Marge.cuidar(" Maggie");
+            Lisa.tocar (" saxofone");
+            Bart.zoar(" com o Skinner");
+            Maggie.Chupar(" chupeta");
+        }
+    }
+}
---
+++ src/PersonagensSimpsons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auladeclasses.src
+{
+    public class PersonagensSimpsons
+    {
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cabelo { get; set; }
+
+        public PersonagensSimpsons(string _nome, int _idade, string _cabelo)
+
+        {
+            Nome = _nome;
+            Idade = _idade;
+            Cabelo = _cabelo;
+        }
+
+        public void beber(string cerveja)
+        {
+            Console.WriteLine($"Estou bebendo{cerveja}");
+        }
+        public void cuidar(string Maggie)
+        {
+            Console.WriteLine($"Estou cuidando da{Maggie}");
+        }
+        public void tocar(string saxofone)
+        {
+            Console.WriteLine($"Estou tocando{saxofone}");
+        }
+        public void zoar(string Skinner)
+        {
+            Console.WriteLine($"Estou zoando{Skinner}");
+        }
+        public void Chupar(string Chupeta)
+        {
+            Console.WriteLine($"Estou chupando{Chupeta}");
+        }
+
+
+    }
+
+}
+
